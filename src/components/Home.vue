cbadd0fd37bc061e6895c401e1998c4984c66b3c
ArielleBishop <ArielleEBishop@gmail.com>
2019-07-27 12:38:48 +0200

    init

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,34 @@
 <template>
-  <h1>Home Page</h1>
+  <carousel-full-screen v-bind:slides="carouselProps"/>
 </template>
 
 <script>
-  export default {
-    name: "Home"
+import CarouselFullScreen from './widgets/CarouselFullScreen.vue'
+
+let TwoD = {
+    title: "2D",
+    imageSrc: ""
+};
+let ThreeD = {
+  title: "3D",
+  imageSrc: ""
+};
+let Video = {
+  title: "Video",
+  imageSrc: ""
+};
+let Programming = {
+  title: "Programming",
+  imageSrc: ""
+};
+
+export default {
+  name: "Home",
+  components: { CarouselFullScreen },
+  data() {
+    return {
+      carouselProps: [TwoD, ThreeD, Video, Programming]
+    }
   }
+}
 </script>
