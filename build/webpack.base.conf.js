79d042268bec7fc048c1acae5ad057281d9a0de4
ArielleBishop <ArielleEBishop@gmail.com>
2019-08-09 13:15:22 -0700

    rename repo

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -28,9 +28,7 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/portfolio/'
-      : '/'
+    publicPath: '/'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
