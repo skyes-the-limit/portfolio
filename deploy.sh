bcbffdfe8eae9542a6f38ae9acd3aea950e61b60
ArielleBishop <33358529+ArielleBishop@users.noreply.github.com>
2019-08-09 13:28:41 -0700

    rename repo (#6)
    * rename repo
    
    * Update Arielle_Bishop_Resume.pdf

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ArielleBishop/portfolio.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:ArielleBishop/ArielleBishop.github.io.git master
 
 cd -
