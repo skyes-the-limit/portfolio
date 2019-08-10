f71f0c487c7ec8bff92b8ba96c08de5ed37590fc
ArielleBishop <ArielleEBishop@gmail.com>
2019-08-10 14:02:17 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'build.js',
+    filename: './build.js',
     publicPath: process.env.NODE_ENV === 'production'
       ? '/portfolio/'
       : '/'
