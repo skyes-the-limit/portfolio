173f12d8b103b4bc0bb21e154f42c74a8f1b6474
ArielleBishop <33358529+ArielleBishop@users.noreply.github.com>
2020-04-12 18:12:13 -0400

    Update CarouselFullScreen.vue

--- src/components/widgets/CarouselFullScreen.vue
+++ src/components/widgets/CarouselFullScreen.vue
@@ -1,29 +1,29 @@
 <template>
-  <ul class="carousel" v-touch:swipe="handleSwipe">
-
-    <input v-for="(slide, index) in slides" :id="slide.page" :key="slide.page"
-           v-model="selectedIndex" :value="index"
-           class="carousel__activator" type="radio" name="activator">
-    <div v-if="slides.length > 1">
-      <label class="carousel__control carousel__control--backward"
-             :for="selectedIndex === 0 ? slides[slides.length - 1].page : slides[selectedIndex - 1].page" />
-      <label class="carousel__control carousel__control--forward"
-             :for="selectedIndex === slides.length - 1 ? slides[0].page : slides[selectedIndex + 1].page" />
+  <div v-touch:swipe="handleSwipe">
+    <v-carousel
+      v-model="selectedIndex"
+      continuous
+      :show-arrows="false"
+      hide-delimiters
+      :height="height"
+      >
+      <v-carousel-item
+        v-for="(slide, index) in slides"
+        :key="index"
+        :src="require('@/assets/img/' + slide.imageSrc)"
+      >
+        <router-link :to="{ name: slide.page }">
+          <div class="title">
+            <h1 class="unselectable">{{ slide.title }}</h1>
+          </div>
+        </router-link>
+      </v-carousel-item>
+    </v-carousel>
+    <div class="controls">
+      <label class="carousel__control carousel__control--backward" @click="selectedIndex--" />
+      <label class="carousel__control carousel__control--forward" @click="selectedIndex++" />
     </div>
-    <li v-for="(slide, index) in slides" :key="slide.imageSrc" class="carousel__slide"
-        :style="{
-          backgroundImage: 'url(' + require('@/assets/img/' + slide.imageSrc) + ')',
-          transform: 'translateX(' + (index - selectedIndex) * 100 + '%)'
-        }">
-      <router-link :to="{ name: slide.page }">
-        <h1>{{ slide.title }}</h1>
-      </router-link>
-    </li>
-    <div class="carousel__indicators">
-      <label v-for="slide in slides" :key="slide.page"
-             class="carousel__indicator" :for="slide.page" />
-    </div>
-  </ul>
+  </div>
 </template>
 
 <script>
@@ -35,9 +35,26 @@
         default: []
       }
     },
+    computed: {
+      width() {
+        return window.innerWidth;
+      },
+      height() {
+        return window.innerHeight - 56;
+      }
+    },
     data() {
       return {
         selectedIndex: 0
+      }
+    },
+    watch: {
+      selectedIndex: function (newVal) {
+        if (newVal === -1) {
+          this.selectedIndex = this.slides.length - 1;
+        } else if (newVal === this.selectedIndex.length) {
+          this.selectedIndex = 0;
+        }
       }
     },
     created() {
@@ -52,81 +69,63 @@
       handleSwipe(direction) {
         switch (direction){
           case "left":
-            this.increment();
+            this.selectedIndex++;
             break;
           case "right":
-            this.decrement();
+            this.selectedIndex--;
             break;
         }
       },
       handleKey(event) {
         switch (event.code) {
           case "ArrowLeft":
-            this.decrement();
+            this.selectedIndex--;
             break;
           case "ArrowRight":
-            this.increment();
+            this.selectedIndex++;
             break;
         }
-      },
-      increment() {
-        this.selectedIndex === this.slides.length - 1 ? this.selectedIndex = 0 : this.selectedIndex ++;
-      },
-      decrement() {
-        this.selectedIndex === 0 ? this.selectedIndex = this.slides.length - 1 : this.selectedIndex--;
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .carousel {
-    height: calc(100vh - 56px);
-    width: 100vw;
-    margin: 0;
-    overflow: hidden;
-    text-align: center;
-    position: relative;
-    padding: 0;
-    list-style: none;
-  }
-
-  .carousel__activator {
-    display: none;
-  }
-
-  /* CHECKED -------------------------------------------------------------------------------------------------------- */
-
-  .carousel__activator:checked ~ .carousel__slide {
-    transition: opacity 0.5s, transform 0.5s, -webkit-transform 0.5s;
-    top: 0;
-    left: 0;
-    right: 0;
-    opacity: 1;
-    -webkit-transform: scale(1);
-    transform: scale(1);
+  .title {
+    position: absolute;
+    top: 0; right: 0; bottom: 0; left: 0;
     display: flex;
     justify-content: center;
+    align-items: center;
   }
 
-  .carousel__activator:checked ~ .carousel__indicators .carousel__indicator {
-    opacity: 1;
+  .title h1 {
+    color: #FFFFFF;
+    font-size: 44px;
+    line-height: 50px;
+    margin: 0;
   }
 
-  /* UNCHECKED ------------------------------------------------------------------------------------------------------ */
+  .unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
 
   .carousel__control {
     height: 30px;
     width: 30px;
-    margin: -15px 15px 0;
+    margin: 15px;
     top: 50%;
     position: absolute;
     display: block;
     cursor: pointer;
     border-width: 5px 5px 0 0;
     border-style: solid;
-    border-color: #fafafa;
-    opacity: 0.35;
+    border-color: rgba(255, 255, 255, 0.35);
     outline: 0;
     z-index: 3;
   }
@@ -146,89 +145,11 @@
     -webkit-transform: rotate(45deg);
     transform: rotate(45deg);
   }
-
-  .carousel__indicators {
-    position: absolute;
-    bottom: 20px;
-    width: 100%;
-    text-align: center;
-  }
-
-  .carousel__indicator {
-    height: 15px;
-    width: 15px;
-    border-radius: 100%;
-    display: inline-block;
-    z-index: 2;
-    cursor: pointer;
-    opacity: 0.35;
-    margin: 0 2.5px 0 2.5px;
-  }
-
-  .carousel__indicator:hover {
-    opacity: 0.75;
-  }
-
-  .carousel__track .carousel__slide {
-    display: block;
-    top: 0;
-    left: 0;
-    right: 0;
-    opacity: 1;
-  }
-
-  .carousel--scale .carousel__slide {
-    -webkit-transform: scale(0);
-    transform: scale(0);
-  }
-
-  .carousel__slide {
+</style>
+<style>
+  body, html {
     height: 100%;
-    position: absolute;
-    overflow-y: auto;
-    opacity: 0;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  .carousel__slide {
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .carousel--thumb .carousel__indicator {
-    height: 30px;
-    width: 30px;
-  }
-
-  h1 {
-    color: #FFFFFF;
-    font-size: 44px;
-    line-height: 50px;
-    text-align: center;
-    margin: 0;
-  }
-
-  a {
-    text-decoration: none;
-    width: 80%
-  }
-
-  .carousel__indicator {
-    background-color: #fafafa;
-  }
-
-  .carousel__slide,
-  .carousel--thumb .carousel__indicators .carousel__indicator {
-    background-size: cover;
-    background-position: center;
-  }
-
-  body {
+    width: 100%;
     overflow: hidden;
   }
 </style>
