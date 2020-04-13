1fb30194892927f2cdf81665d652b14b4e2376c9
ArielleBishop <33358529+ArielleBishop@users.noreply.github.com>
2020-04-13 09:59:05 -0400

    Add X to escape modal

--- src/components/widgets/Modal.vue
+++ src/components/widgets/Modal.vue
@@ -8,6 +8,9 @@
               <label class="carousel__control carousel__control--backward" @click="selectedIndex--" />
               <label class="carousel__control carousel__control--forward" @click="selectedIndex++" />
             </template>
+            <svg @click.self="closeModal()" viewBox="0 0 24 24">
+              <path fill="#303030" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+            </svg>
             <vimeo-player v-if="displayObject.type === 'video'" :video-id="displayObject.source"
                           :player-width="playerWidth" :player-height="playerHeight"
                           :loop="loop" :autoplay="autoplay" />
@@ -158,6 +161,14 @@
     max-width: calc(100vw - 150px);
     max-height: 75vh;
     object-fit: contain;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+    float: right;
+    margin: -16px;
+    cursor: pointer;
   }
 
   .annotations {
