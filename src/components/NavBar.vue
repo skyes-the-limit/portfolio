db69297f28b09b963fd569b822e097d75e64a372
ArielleBishop <33358529+ArielleBishop@users.noreply.github.com>
2020-04-14 16:48:14 -0400

    Use Vuetify NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -16,142 +16,128 @@
     </span>
   </div>
   <div v-else class="nav-bar">
-    <div class="burger-button">
-      <Slide :closeOnNavigation="true">
-        <router-link :to="{ name: 'Home' }">
-          <span>Home</span>
-        </router-link>
-        <router-link :to="{ name: 'Illustration' }">
-          <span>Illustration</span>
-        </router-link>
-        <router-link :to="{ name: 'CGI' }">
-          <span>CGI</span>
-        </router-link>
-        <router-link :to="{ name: 'Video' }">
-          <span>Video</span>
-        </router-link>
-        <router-link :to="{ name: 'Software' }">
-          <span>Software</span>
-        </router-link>
-        <router-link :to="{ name: 'About' }">
-          <span>About</span>
-        </router-link>
-      </Slide>
-    </div>
+    <svg class="drawer-button" @click="drawer = true" viewBox="0 0 24 24">
+      <path fill="#fff" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+    </svg>
     <span class="nav-bar__logo">
-        <router-link :to="{ name: 'Home' }">Arielle Bishop</router-link>
+      <router-link :to="{ name: 'Home' }">Arielle Bishop</router-link>
     </span>
+    <v-navigation-drawer v-model="drawer" absolute temporary dark>
+      <v-list>
+        <v-list-item>
+          <router-link :to="{ name: 'Home' }">Home</router-link>
+        </v-list-item>
+        <v-list-item>
+          <router-link :to="{ name: 'Illustration' }">Illustration</router-link>
+        </v-list-item>
+        <v-list-item>
+          <router-link :to="{ name: 'CGI' }">CGI</router-link>
+        </v-list-item>
+        <v-list-item>
+          <router-link :to="{ name: 'Video' }">Video</router-link>
+        </v-list-item>
+        <v-list-item>
+          <router-link :to="{ name: 'Software' }">Software</router-link>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-  import { Slide } from 'vue-burger-menu'
-
-  export default {
-    name: 'NavBar',
-    components: { Slide },
-    data() {
-      return {
-        windowWidth: 0
-      }
-    },
-    created() {
-      this.$nextTick(() => {
-        window.addEventListener('resize', this.onResize);
-      });
-      this.onResize();
-    },
-    destroyed() {
-      window.removeEventListener('resize', this.onResize);
-    },
-    methods: {
-      onResize() {
-        this.windowWidth = window.visualViewport.width;
-      }
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      windowWidth: 0,
+      drawer: false
+    };
+  },
+  created() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+    this.onResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
+    onResize() {
+      this.windowWidth = window.visualViewport.width;
     }
   }
+};
 </script>
 
 <style scoped lang="scss">
+.nav-bar {
+  background-color: #303030;
+  height: 56px;
+  font-size: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-bar a {
+  color: #ffffff;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.nav-bar__logo {
+  font-weight: bolder;
+  border-right: 1px solid #dddddd;
+}
+
+.drawer-button {
+  position: relative;
+  visibility: hidden;
+}
+
+@media only screen and (max-width: 484px) {
   .nav-bar {
-    background-color: #303030;
-    height: 56px;
-    font-size: 16px;
-    display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: center;
   }
-
-  .nav-bar a {
-    color: #FFFFFF;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
   .nav-bar__logo {
-    font-weight: bolder;
-    border-right: 1px solid #DDDDDD;
+    font-size: 32px;
+    margin: 0 auto;
+    border: none;
   }
+  .drawer-button {
+    width: 40px;
+    height: 56px;
+    margin-left: 10px;
+    visibility: visible;
+  }
+}
 
-  .burger-button {
-    position: absolute;
-    left: 14px;
-    top: 14px;
-    visibility: hidden;
+@media only screen and (max-width: 320px) {
+  .nav-bar__logo {
+    font-size: 24px;
   }
+}
 
-  @media only screen and (max-width: 484px) {
-    .nav-bar {
-      position: sticky;
-      justify-content: center;
-      align-items: center;
-    }
-    .nav-bar__logo {
-      font-size: 32px;
-      border: none;
-    }
-    .burger-button {
-      visibility: visible;
-    }
+@media only screen and (min-width: 485px) {
+  .nav-bar a {
+    padding: 0 0.5rem;
   }
+}
 
-  @media only screen and (max-width: 320px) {
-    .nav-bar__logo {
-      font-size: 24px;
-    }
+@media only screen and (min-width: 600px) {
+  .nav-bar a {
+    padding: 0 1rem;
   }
+}
 
-  @media only screen and (min-width: 485px) {
-    .nav-bar a {
-      padding: 0 0.5rem;
-    }
+@media only screen and (min-width: 819px) {
+  .nav-bar {
+    font-size: 18px;
   }
-
-  @media only screen and (min-width: 600px) {
-    .nav-bar a {
-      padding: 0 1rem;
-    }
+  .nav-bar a {
+    padding: 0 2rem;
   }
-
-  @media only screen and (min-width: 819px) {
-    .nav-bar {
-      font-size: 18px;
-    }
-    .nav-bar a {
-      padding: 0 2rem;
-    }
-  }
-</style>
-
-<style>
-  .bm-burger-button {
-    left: 0;
-    top: 0;
-  }
-  .bm-burger-bars {
-    background-color: #FFFFFF;
-  }
-  .bm-item-list {
-    color: #303030;
-  }
+}
 </style>
