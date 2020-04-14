c3b96320360a06d0bedf6b012a3f36ccaaf5bd26
ArielleBishop <33358529+ArielleBishop@users.noreply.github.com>
2020-04-14 17:20:57 -0400

    Update About

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,47 @@
 <template>
-  <div class="container">
-    <pdf :src="require('@/assets/doc/Arielle_Bishop_Resume.pdf')" class="pdf" />
+  <div>
+    <img :src="require('@/assets/img/about-profile.png')">
+    <div class="text">
+      <p>I am a Boston based artist and software devleoper currently studying at Northeastern University. 
+        My focus is on full-stack development of web and mobile experiences, with some design mixed in.
+        I am motivated by projects that are focused on the human element, where I can have a visible impact.</p>
+      <br />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        :href="require('@/assets/doc/Arielle_Bishop_Resume.pdf')"
+      >Resume</a>
+    </div>
   </div>
 </template>
 
 <script>
-  import pdf from 'vue-pdf'
-
-  export default {
-    name: 'About',
-    components: { pdf }
-  }
+export default {
+  name: "About"
+};
 </script>
 
 <style scoped lang="scss">
-  .container {
+  .text {
+    margin: 0 auto;
     display: flex;
-    justify-content: center;
-    margin: 2rem;
+    flex-direction: column;
+    align-items: center;
+    max-width: calc(max(50%, 600px));
+    padding: 24px;
+  }
+  
+  img {
+    width: 100vw;
+    height: 60vh;
+    object-fit: cover;
+    object-position: right top;
+    opacity: .9;
   }
 
-  .pdf {
-    width: 100%;
+  @media only screen and (max-height: 811px) {
+    img {
+      height: 40vh
+    }
   }
 </style>
